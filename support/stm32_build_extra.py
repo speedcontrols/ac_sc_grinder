24eba36e3c3c48c03d976829382f7f6ada63cc11
Vitaly Puzrin <vitaly@rcdesign.ru>
2020-04-29 17:42:25 +0300

    define ldscript without py file

--- support/stm32_build_extra.py
+++ support/stm32_build_extra.py
@@ -8,12 +8,5 @@ for e in [ env, projenv ]:
     e.Append(LINKFLAGS = [ "--specs=nano.specs", "--specs=nosys.specs" ])
     e.Replace(AS = '$CC', ASCOM = '$ASPPCOM')
 
-    #
-    # .ld script should be passed to linker directly as "-T<path>"
-    # instead of "-Wl,-T<path>", to avoid 1K RAM loss for ".data.impure_data"
-    #
-    e.Replace(BUILD_FLAGS = [i for i in e['BUILD_FLAGS'] if not i.startswith('-Wl,-T')])
-    e.Replace(LINKFLAGS = [(i[4:] if i.startswith('-Wl,-T') else i) for i in e['LINKFLAGS']])
-
 #print('=====================================')
 #print(env.Dump())
