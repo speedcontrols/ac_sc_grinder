9eda2b29caf577b5009497e5f20b45cae9c87ca8
Vitaly Puzrin <vitaly@rcdesign.ru>
2020-06-27 09:31:38 +0300

    Drop unused generaror of eeprom map

--- src/app.h
+++ src/app.h
@@ -3,14 +3,6 @@
 
 #include <stdint.h>
 
-// "EEPROM" location and size for RPM non-linearity conmensation table.
-// Starts after other config variables.
-#define CFG_RPM_INTERP_TABLE_START_ADDR 10
-#define CFG_RPM_INTERP_TABLE_LENGTH 16
-
-#define CFG_R_INTERP_TABLE_START_ADDR 26
-#define CFG_R_INTERP_TABLE_LENGTH 7
-
 float eeprom_float_read(uint32_t addr, float dflt);
 void eeprom_float_write(uint32_t addr, float val);
 
