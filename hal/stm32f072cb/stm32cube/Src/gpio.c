a4365fa895912b4e19041ab9c847c4f1ab66fb61
Vitaly Puzrin <vitaly@rcdesign.ru>
2020-09-12 23:57:26 +0300

    Set GPIO speed low

--- hal/stm32f072cb/stm32cube/Src/gpio.c
+++ hal/stm32f072cb/stm32cube/Src/gpio.c
@@ -56,7 +56,7 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PB12 */
